d17ed6a09c939c92fd42f247c2d77b6ce3491a88
Pamela <pamelarane@gmail.com>
2021-04-20 16:24:42 -0300

    aula10

---
+++ operadores/index.js
@@ -0,0 +1,42 @@
+/* console.log('Olá, Consolee!')
+
+//1)
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) // false
+
+resultado = bool1 && bool2 && bool3
+console.log("b. ", resultado)//false
+
+resultado = !resultado && (bool1 || bool1)
+console.log("c. ", resultado)// true
+
+console.log("e. ", typeof resultado)//false 
+ */
+
+//2)
+
+/*  */
+
+
+// exercios e ficçao 
+
+//1)
+
+//a) prompt("Tem quantos anos")
+//b) prompt("Quantos anos tem seus melhores amigos")
+
+
+
+
+//2)
+prompt("insira um número par")
+
+
+
+
+
+
+//4
+
+
