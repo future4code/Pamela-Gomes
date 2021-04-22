88bcd687af5379a568a7214c10e497d754e20d69
Pamela <pamelarane@gmail.com>
2021-04-22 13:22:36 -0300

    semana3-aula12

---
+++ condicionais/index.js
@@ -0,0 +1,69 @@
+//EXERCÍCIO 1 //
+
+// o codigo mostra um formularios para o usario e nele, ele iria preencher, um numero aleatorio para testar.
+
+// so vai aparecer passou no teste quando o usario colocar o numero 2
+
+// vai aparecer  que nao passou se o usuario colocar o numero maior que 2
+
+//EXERCÍCIO 2//
+
+//a. Para que serve o código acima?
+
+ /* O codigo serve para indenficar o que fruta o usario escolher  */
+
+//Qual será a mensagem impressa no console, se o valor de fruta for "Maçã"
+// o preço da fruta maça e maça 
+
+/* c. Considere que um usuário queira comprar uma `Pêra`, qual seria a mensagem impressa no console se retirássemos o
+ `break` que está logo acima do `default` (o `break` indicado pelo comentário "BREAK PARA O ITEM c.")? */
+ //nao sei.
+
+
+//EXERCÍCIO 3//
+
+//a. O que a primeira linha está fazendo?
+
+
+
+//////////Exercícios de escrita de código//////
+
+//EXERCÍCIO 4
+
+/* const respostaDoUsuario = prompt("Quantos anos voce tem ? :)")
+const numero = Number(respostaDoUsuario)
+
+if(numero < 59 === 60) {
+  console.log("Você pode dirigir.")
+} else {
+  console.log("Você não pode dirigir.")
+} */
+
+//EXERCÍCIO 5//
+
+
+/* const respostaDousuario = prompt(" Em que turo do dia você estudo ?. M (Matutino) para V (Verpertino) ou N (Noturno) :)")
+
+if((respostaDousuario = M) ){
+  console.log("Bom dia!.")
+} {
+  console.log("Você não pode dirigir.")
+}  */
+
+
+//EXERCÍCIO 6
+
+
+/* let turno = prompt("Em que turo do dia você estudo?. M (matutino) ou V (Vespertino) ou N (Noturno)")
+let mensagem
+switch (turno) {
+  case "M":
+   
+    break;
+  case "V":
+    
+    break;
+  case "N":
+    
+}
+console.log( "Bom Dia! ", turno )  */
