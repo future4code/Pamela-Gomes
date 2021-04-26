eaee51a1f5ab237d9a40dba8c38529c0261b55aa
Pamela <pamelarane@gmail.com>
2021-04-26 16:23:08 -0300

    semana4-aula14

---
+++ semana4/funçoes/index.js
@@ -0,0 +1,110 @@
+           /* Interpretaçao de codigos */
+
+//-------------- EXERCÍCIO 1 -------------
+
+//a) no console vai aparece o resultado de 2*5 = 10 e 10*5 = 5
+//10
+//50
+
+//b) no momento que tirei nao apareceu nada e como se nao tivesse chamado a funçao
+
+/* function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+minhaFuncao(2)
+minhaFuncao(10)
+ */
+
+
+//-------------- EXERCÍCIO 2 -------------
+
+//a) as saidas no console 
+
+//b)apareceu caio 
+
+/* let arrayDeNomes = ["Darvas", "Caio"];
+
+const outraFuncao = function(array) {
+	for (let i = 0; i < 2; i++) {
+		console.log(array[i])
+	}
+}
+
+
+outraFuncao(arrayDeNomes)
+ */
+
+
+//-------------- EXERCÍCIO 3 -------------
+/*  */
+
+/* const metodo = (array) => {
+    let arrayFinal = [];
+  
+    for (let x of array) {
+          if (x % 2 === 0) {
+          arrayFinal.push(x * x)
+          }
+    }
+  
+    return arrayFinal;
+  } */
+
+    /* Exercícios de escrita de código */
+
+//-------------- EXERCÍCIO 4 ------------- 
+
+//a)
+
+/* function euSou() {
+    console.log("eu sou Pamela Rane,tenho 23 anos, moro em São Paulo e sou estudante")
+}
+euSou() 
+ */
+
+//b)
+
+/* function dadosUsuario(nome, idade, local, profissao){
+    nome=prompt("Qual e seu nome ?")
+    idade=Number(prompt("Quantos anos voce tem?"))
+    local=prompt("qual e a cidade que voce mora?")
+    if()
+}
+console.log('eu sou ${nome}, tenho ${idade} anos, moro na cidade ${local} e sou ${profissao}')
+
+ */
+
+//------------- EXERCÍCIO 5 ----------------
+
+//a)
+
+/* function somaDoisNumeros(num1, num2){
+    const soma = num1 + num2
+    return soma
+}
+
+const resuldado = somaDoisNumeros(8, 1)
+const resultado2 = somaDoisNumeros(resuldado, 2)
+console.log (resultado2)
+ */
+
+//b)
+
+/* function somaDoisNumeros(num1, num2){
+    const soma = num1 >= num2
+    return soma
+}
+
+const resuldado = somaDoisNumeros(5, 1)
+const resultado2 = somaDoisNumeros(resuldado, 2)
+console.log (resultado2) */
+
+//c)
+
+/*  function questaoc(hii) {
+    console.log("")
+ */
+
+
+//------------- EXERCÍCIO 6 ----------------
