e9bbde590c46e8705a168ee66e9965909734b09a
Pamela <pamelarane@gmail.com>
2021-04-28 15:54:36 -0300

    mensagem

---
+++ aula-16-blog/index.js
@@ -0,0 +1,16 @@
+function criarPosts(){
+    const conteudo = document.getElementById("conteudo-post")
+    const autor = document.getElementById("autor-post")
+    const titulo = document.getElementById("titulo-post")
+    const text = document.getElementById("container-de-posts")
+    const textoValor = document.getElementById("texto").nodeValue
+    const posts = document.getElementById("posts")
+
+    
+}
+
+/* const conteudoDoPost = {
+    titulo: tituloDoPost.value
+    autor: autorCriar.value
+    conteudo: conteudoAdd.value
+} */
